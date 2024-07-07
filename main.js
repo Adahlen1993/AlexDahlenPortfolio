737539d05287d1c5690ca8ff15a692a49eeae885
Adahlen1993 <156604329+Adahlen1993@users.noreply.github.com>
2024-07-06 22:20:24 -0500

    personal website

---
+++ main.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  VANTA.NET({
+    el: "#vanta-bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.0,
+    minWidth: 200.0,
+    scale: 1.0,
+    scaleMobile: 1.0,
+    color: 0x3fffff,
+    points: 7.0,
+  });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
